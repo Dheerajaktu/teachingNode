d27b3a28777e688880d68b6c1ba74757cc708d67
dheerajaktu <dheerajsharmakcmt@gmail.com>
2023-08-30 23:50:54 +0530

    added new sign up page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+//router for register page
+router.get('/signup', function(req, res, next){
+  res.render('signup', { title: 'Sign up Form' })
+})
+
+
+
+// router.get('/home', function(req, res, next){
+//   res.render('index', { title: 'Sign up Form' })
+// })
+
 module.exports = router;
