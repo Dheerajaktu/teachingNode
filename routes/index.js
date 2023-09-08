f04ddc0db78f07ad65869a785926a9e1977cf7fd
dheearjaktu <dheearjaktu>
2023-09-08 21:23:23 +0530

    login form changes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+  console.log("bhawish",req.body,req.params)
   res.render('index', { title: 'Express' });
 });
 
