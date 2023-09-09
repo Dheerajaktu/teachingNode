cb6773029265a992597ed4ed42fcc8d123c7067d
dheerajaktu <dheerajsharmakcmt@gmail.com>
2023-09-09 22:27:48 +0530

    login success with homepage

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -18,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("bhawish GET",req.body,req.params)
+  console.log("GET method called")
   res.render('index', { title: 'Express' });
 });
 
+// home page
+router.get('/home', function(req, res){
+  console.log("home route called");
+  res.render('home', {title : 'Home'});
+})
+
+
 // for post data 
 router.post('/', function(req, res, next) {
-  console.log("bhawish POST",req.body);
+  console.log("POST method called",req.body);
   const email = req.body.email; 
   const pass = req.body.password;
 
@@ -20,11 +27,12 @@ router.post('/', function(req, res, next) {
   // server logic for login
 
   if(email != 'Test@123.com' || pass != '12345'){
-    return res.json({message : 'Error in Username or Password, Please try again..'});
-    // res.render('index', { title: 'Express' });
+    // return res.json({message : 'Error in Username or Password, Please try again..'});
+    res.render('error', { title: 'Login Fail..' });
   }else {
     // return res.json({message : 'Login Success..'});
-    res.render('home', { title: 'Login Success' });
+    // res.render('home', { title: 'Login Success' });
+    res.redirect('/home');
   }
 
 
