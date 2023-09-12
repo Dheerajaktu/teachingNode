a7239154195b8de93c50d590090fb344b0b3b121
dheerajaktu <dheerajsharmakcmt@gmail.com>
2023-09-12 22:30:59 +0530

    Interview question

---
+++ DSA/Array/a1.js
@@ -0,0 +1,35 @@
+/** 
+ * #1 - Array sorting
+ * #2 - Array sorting in Asc and desc
+ * #3 - Array manipulate 
+ * #4 - Array traverse 
+ * $5 - Find duplicate values in array 
+ * #6 - find missing values in array
+ * #7 - find index of two sum
+ * #8 - other index based question
+*/
+
+//#1 
+
+//const arr = [3, 4, 2, 3, 6, 32, 3, 7, 9, 45, 3] find the prime numbers in this array;
+
+// #2
+
+let a = 10;
+let b = 20;
+//output b = 10 & a = 20
+
+// swap the values 
+let c = a; // c = 10
+a = b // a = 20;
+b = c // b = 10;
+
+// 
+
+console.log('values of a and b is', a , b);
+
+// javascript 
+
+// functions 
+// objects
+// oops 
